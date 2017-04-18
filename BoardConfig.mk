cb01e30935caf5337b36df9fdd2b9ee9956d48e3
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    beryllium: Enforce common device & vendor tree presence
    A compiled build has no chance of booting without the common device setup
    and the prebuilt binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from sdm845-common
--include device/xiaomi/sdm845-common/BoardConfigCommon.mk
+include device/xiaomi/sdm845-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/beryllium
 
@@ -42,4 +42,4 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Inherit from the proprietary version
--include vendor/xiaomi/beryllium/BoardConfigVendor.mk
+include vendor/xiaomi/beryllium/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/xiaomi/beryllium/beryllium-vendor.mk)
+$(call inherit-product, vendor/xiaomi/beryllium/beryllium-vendor.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2246
