79960051364b037fb9d1481291e46133dd351eb3
LuK1337 <priv.luk@gmail.com>
2018-03-31 18:47:46 +0200

    beryllium: Configure FM
    Change-Id: I413105b4da8bf10cac0c74ccee06b8ab331dfab3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,8 +12,15 @@ DEVICE_PATH := device/xiaomi/beryllium
 # Assert
 TARGET_OTA_ASSERT_DEVICE := beryllium
 
+# Audio
+AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
+
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
+
+# FM
+BOARD_HAS_QCA_FM_SOC := "cherokee"
+BOARD_HAVE_QCOM_FM := true
 
 # Kernel
 TARGET_KERNEL_CONFIG := beryllium_defconfig
