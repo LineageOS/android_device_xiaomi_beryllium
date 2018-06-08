23915076cc50c5f7d41545ca0deed70e26fec134
Bruno Martins <bgcngm@gmail.com>
2018-06-08 12:08:37 +0100

    beryllium: overlay-lineage: Configure hardware keys
    Change-Id: I1f1ab1e359f00e9dcaedf9d4f03b43da863816d2

--- device.mk
+++ device.mk
@@ -15,7 +15,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
