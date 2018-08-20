872155d626afe4d79a5e82ac251aaadb855a27b8
Adrian Roos <roosa@google.com>
2018-08-20 16:13:04 +0200

    beryllium: Add overlay for masking the notch
    Bug: 112876936
    Test: Go to Settings > Developer Options > Simulate display with cutout > "No cutout". Cutout should be hidden. Rotate screen, take screenshots, screenrecord should all work.
    Change-Id: I66684d8254fd3c1ee6851e67c0b09d176d4c02af
    (cherry picked from commit dcc48dc9071c37c8b9fe7caee96585d20d149060)

---
+++ overlay/packages/apps/overlays/NoCutoutOverlay/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := DisplayCutoutNoCutout
+LOCAL_CERTIFICATE := platform
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+LOCAL_PACKAGE_NAME := NoCutoutOverlay
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_RRO_PACKAGE)
