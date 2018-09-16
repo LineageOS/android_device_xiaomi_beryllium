591f996eb28956b3f695717fc23084ced929c3ff
TheScarastic <warabhishek@gmail.com>
2018-09-16 14:34:59 -0400

    beryllium: Add stock system props
    Change-Id: Ib39197d512644c4aff916c4486a6f7f5232aae25

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Properties
+-include $(LOCAL_PATH)/device-props.mk
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
