d22b503e62b8ded3c79b859d8b55843d6fd6733b
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 17:11:10 +0200

    beryllium: Make Android.mk search recursive
    Change-Id: I3a05344f949eae581f1f17cc33b1df475d3e6760

--- Android.mk
+++ Android.mk
@@ -16,5 +16,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),beryllium)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
