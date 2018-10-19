4dee84b800fbe4dc20973010314b58aacd0eeb3f
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 16:59:43 +0200

    beryllium: Build NoCutoutOverlay
    Change-Id: I45edeb3ae20f374cd4510bad83b3a51059a7e743

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # Properties
 -include $(LOCAL_PATH)/device-props.mk
 
