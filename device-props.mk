b68445ff3d1d1c1d458e1f9fd334882ac8496fb7
TheScarastic <warabhishek@gmail.com>
2018-11-02 16:17:49 -0400

    beryllium: Commonize props
    Change-Id: Ifc73e5bb73fab83fa6a46eb5fdd8748066e58255

--- device-props.mk
+++ device-props.mk
@@ -1,16 +1,3 @@
-# Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.camera.enableAdvanceFeatures=0x47 \
-    persist.vendor.camera.multicam.fpsmatch=TRUE \
-    persist.vendor.camera.multicam.hwsync=TRUE \
-    persist.vendor.camera.multicam.framesync=1 \
-    persist.vendor.camera.perfcapture=1
-
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qcom.ad=1 \
-    ro.qcom.ad.calib.data=/vendor/etc/sdr_config.cfg \
-    ro.qcom.ad.hdr.calib.data=/vendor/etc/hdr_config.cfg \
-    ro.qcom.ad.sensortype=2 \
-    ro.qualcomm.cabl=0 \
-    ro.sf.lcd_density=480
+    ro.sf.lcd_density=440
