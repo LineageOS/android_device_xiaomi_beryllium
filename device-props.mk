3f033c2d996c6936af75f8ec6bc697d66ca7e21e
Bruno Martins <bgcngm@gmail.com>
2019-02-17 21:45:30 +0000

    beryllium: Set device stock props
    Change-Id: Ie2ae40d4813f2e43813461f1279087677e10eb63

--- device-props.mk
+++ device-props.mk
@@ -5,3 +5,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=440
+
+# Display postprocessing
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.ppd.fde.config=0 \
+    vendor.display.enable_default_color_mode=0
+
+# FM
+#PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.hw.fm.init=0
