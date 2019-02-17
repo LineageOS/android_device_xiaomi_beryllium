858cca33c231b60f71a75c08c377de26380bc902
Bruno Martins <bgcngm@gmail.com>
2019-02-17 21:45:30 +0000

    beryllium: Import device-specific stock props
    Change-Id: Ie2ae40d4813f2e43813461f1279087677e10eb63

--- device-props.mk
+++ device-props.mk
@@ -5,3 +5,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=440
+
+# Beryllium only
+#PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.hw.fm.init=0 \
+    persist.ppd.fde.config=0 \
+    vendor.display.enable_default_color_mode=0
