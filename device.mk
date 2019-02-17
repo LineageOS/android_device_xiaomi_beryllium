b212cdd60e793930514b6a2f570a26b8ff6bc125
Bruno Martins <bgcngm@gmail.com>
2019-02-17 19:34:27 +0000

    beryllium: Copy device fstab to vendor
    Change-Id: I63e6126d48a2efb586b80567f80a1498f0b81cb7

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Inherit from sdm845-common
 $(call inherit-product, device/xiaomi/sdm845-common/sdm845.mk)
 
+# Device fstab
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc \
