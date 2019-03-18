fe291a653688179ebc62dcdea66669ab0d0aa82b
Bruno Martins <bgcngm@gmail.com>
2019-03-18 21:48:09 +0000

    beryllium: Build PocketMode app
    Change-Id: Ia7bde817fbd4ac854ef8b9a6d54e9e597487ee5a

--- device.mk
+++ device.mk
@@ -47,3 +47,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
+
+# PocketMode
+PRODUCT_PACKAGES += \
+    XiaomiPocketMode
