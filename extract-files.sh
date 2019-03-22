6556aa0357dd330cc302bf1e1304e2cf7e08a83c
Vladimir Oltean <olteanv@gmail.com>
2019-03-22 22:45:03 +0000

    beryllium: Update for latest common extractors
    Change-Id: I4a8f81b2eb05dafc2b022daddb210eb3100e36f0

--- extract-files.sh
+++ extract-files.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
 
+# Required!
 export DEVICE=beryllium
 export DEVICE_COMMON=sdm845-common
 export VENDOR=xiaomi
 
 export DEVICE_BRINGUP_YEAR=2018
 
-./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
+# Required!
 export DEVICE=beryllium
 export DEVICE_COMMON=sdm845-common
 export VENDOR=xiaomi
 
 export DEVICE_BRINGUP_YEAR=2018
 
-./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
