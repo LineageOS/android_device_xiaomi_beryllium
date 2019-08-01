82b466dd531607e0b0b5d904c36400520f13bde0
TheScarastic <warabhishek@gmail.com>
2019-08-01 17:14:38 +0530

    beryllium: Patch libs to get it working with VNDK isolation
    Change-Id: I2cbfd216ce176de650278e23ccd2a57be44d596d

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Sourced by the common device repo when extracting device-specific blobs
+function blob_fixup() {
+    case "${1}" in
+    vendor/lib/libmorpho_video_refiner.so)
+        patchelf --replace-needed "libstdc++.so"  "libc++.so" "${2}"
+        ;;
+    vendor/lib64/libgf_hal.so)
+        patchelf --remove-needed "libpowermanager.so" "${2}"
+        ;;
+    vendor/lib64/libremosaiclib.so)
+        patchelf --replace-needed "libstdc++.so"  "libc++.so" "${2}"
+        ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
