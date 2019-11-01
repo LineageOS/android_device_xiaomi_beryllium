0e8c53ac98c4f05bd293d2d3b4baae1f239a639a
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    beryllium: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- device.mk
+++ device.mk
@@ -27,6 +27,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+
 # Properties
 -include $(LOCAL_PATH)/device-props.mk
 
