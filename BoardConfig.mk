c9e2cf7c223da0c617f96bb43e0a395a19562e8a
Nicholas Lim <nicknitewolf@lineageos.org>
2020-05-18 17:06:12 +0800

    beryllium: Drop FDE support entirely
    Change-Id: Ifecd15ee6c86a4571e5a7cc961372348455c4d5c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,6 @@ TARGET_OTA_ASSERT_DEVICE := beryllium
 # Audio
 AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
 
-# Crypto
-TARGET_HW_DISK_ENCRYPTION := true
-
 # FM
 BOARD_HAS_QCA_FM_SOC := "cherokee"
 BOARD_HAVE_QCOM_FM := true
