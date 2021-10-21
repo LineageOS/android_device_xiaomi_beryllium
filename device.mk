96851a30c576b6cf95bf7fe4b5832fcbd72fac70
Ratoriku <a1063021545@gmail.com>
2021-10-21 23:08:08 +0800

    beryllium: Adapt NoCutOutOverlay to S
    * NoCutOutOverlay packages have been moved to fwb in S. That means
      we don't need to keep a copy in the device tree, just directly
      use the one in fwb and adapt config.xml to our devices.
    
    Change-Id: I45b381c7e45f92e60a8608b3c201c5e48892b0ae
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: Bruno Martins <bgcngm@gmail.com>

--- device.mk
+++ device.mk
@@ -24,9 +24,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
