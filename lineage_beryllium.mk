c1c7b9c8158b8e46512e393d91589e3a4924da8a
LuK1337 <priv.luk@gmail.com>
2022-01-27 10:53:26 +0100

    beryllium: Make device name/product prop overrides work
    Change-Id: I64d11103ddb8f9dce70b9bc110745d4aafd667dc

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2021 The LineageOS Project
+# Copyright (C) 2018-2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -16,10 +16,12 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
+PRODUCT_SYSTEM_NAME := beryllium
+
 BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
-    PRODUCT_NAME="beryllium"
+    TARGET_PRODUCT="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
