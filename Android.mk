01cef7ca0195ad0d1530f31a11aa50d5c5482d34
LuK1337 <priv.luk@gmail.com>
2022-03-31 18:05:37 +0200

    Revert "beryllium: Make Android.mk search recursive"
    No longer needed.
    
    This reverts commit d22b503e62b8ded3c79b859d8b55843d6fd6733b.
    
    Change-Id: Iae9c4257a98dd4ba47ef9992b1dec133ccc56764

--- Android.mk
+++ Android.mk
@@ -16,6 +16,5 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),beryllium)
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
