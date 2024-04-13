d3372f4206b0108faa7637b31694c5f06f7f6996
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-04-13 22:28:04 +0200

    beryllium: Opt-in to FM support
    Change-Id: Iecd8c06d260619a7f74373187836db9563312db9

--- device.mk
+++ device.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+TARGET_HAS_FM := true
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
