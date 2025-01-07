6004c92cf527f8a548d77b181b0e23ea90c5876b
Gérard Parat <github@le-cycliste.fr>
2025-01-07 22:56:58 +0100

    beryllium: switch to python extract-utils
    Change-Id: I2e08e4c3dde64fcf9f7f9692d7f85dfe32c17249

---
+++ extract-files.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/xiaomi/sdm845-common'
+]
+
+module = ExtractUtilsModule(
+    'beryllium',
+    'xiaomi',
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sdm845-common', module.vendor)
+    utils.run()
---
+++ setup-makefiles.py
@@ -0,0 +1,1 @@
+#!./extract-files.py --regenerate_makefiles
