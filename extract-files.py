9d6935b47ecee98dfee936324c6af8de0cca7425
Bruno Martins <bgcngm@gmail.com>
2025-03-29 12:51:31 +0000

    beryllium: Setup ELF checks
    Change-Id: Ida6d2d91ed0a722b9cfadd71b6221dac614c33ca

--- extract-files.py
+++ extract-files.py
@@ -8,14 +8,39 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
 namespace_imports = [
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/qcom-caf/sdm845',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/display',
     'vendor/xiaomi/sdm845-common'
 ]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'vendor.qti.hardware.fm@1.0',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libcld80211',
+        'libwifi-hal-ctrl',
+    ): lib_fixup_remove,
+}
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib/camera/components/com.qti.node.watermark.so': blob_fixup()
@@ -25,9 +50,9 @@ blob_fixups: blob_fixups_user_type = {
 module = ExtractUtilsModule(
     'beryllium',
     'xiaomi',
-    namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
-    check_elf=False,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
