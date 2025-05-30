e4dd96e9c44b513f9ff2a226b975df9250953377
duckyduckg <duckyduckg65@gmail.com>
2025-05-30 07:22:38 +0500

    beryllium: remove bluetooth, fm blobs and disable elf check for few blobs
    * they are now in common device tree and updated for k4.19
    
    * also disable elf check for some blobs
      they depend on 32-bit blobs that are not on updated vendor for k4.19
    
    Change-Id: Id62595b473e253caf361a03d3b8af10625512827
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -34,9 +34,6 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
-        'vendor.qti.hardware.fm@1.0',
-    ): lib_fixup_vendor_suffix,
-    (
         'libcld80211',
         'libwifi-hal-ctrl',
     ): lib_fixup_remove,
