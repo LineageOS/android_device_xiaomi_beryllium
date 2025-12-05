42ef87c06d02a4c4b6bb128a58a80587ae5f6c0a
althafvly <althafvly@gmail.com>
2025-12-05 15:50:03 +0530

    beryllium: Add libdng_sdk_shim for camera watermark lib
    Change-Id: I566fc1e4e5909e339da6470b1e5f9969ecc8ce05

--- extract-files.py
+++ extract-files.py
@@ -44,6 +44,7 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib/camera/components/com.qti.node.watermark.so': blob_fixup()
+        .add_needed('libdng_sdk_shim.so')
         .add_needed('libpiex_shim.so'),
     'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
         .clear_symbol_version('__aeabi_memcpy')
